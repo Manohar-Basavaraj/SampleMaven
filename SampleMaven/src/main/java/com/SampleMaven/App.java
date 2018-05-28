dcbede83bf9da340f0ffddb0325aa11fcb1c992b
manohar <manohar.b@softvision.com>
2018-05-28 19:35:51 +0530

    This SampleMaven Commit

---
+++ SampleMaven/src/main/java/com/SampleMaven/App.java
@@ -0,0 +1,12 @@
+package com.SampleMaven;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Jenkins!!");
+
+	}
+
+}
